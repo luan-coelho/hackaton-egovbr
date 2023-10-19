1e89d2d19c27cbfd6868c51b4f0ac580f6568e7a
Kaio Macedo <kaiomm2000@gmail.com>
2023-10-19 18:23:55 -0300

    respostas a pergunta adicionadas

---
+++ backend/hackaton-egovbr/src/main/java/br/unitins/hackaton/egovbr/model/Resposta.java
@@ -0,0 +1,23 @@
+package br.unitins.hackaton.egovbr.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Resposta extends EntityClass {
+
+    @ManyToOne
+    private Pergunta pergunta;
+
+    @ManyToMany
+    private Alternativa alternativa;
+
+}
---
+++ backend/hackaton-egovbr/src/main/java/br/unitins/hackaton/egovbr/model/RespostaAvaliacao.java
@@ -0,0 +1,28 @@
+package br.unitins.hackaton.egovbr.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+public class RespostaAvaliacao extends EntityClass {
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Avaliacao avaliacao;
+
+    @OneToMany
+    private List<Resposta> respostas;
+
+}
