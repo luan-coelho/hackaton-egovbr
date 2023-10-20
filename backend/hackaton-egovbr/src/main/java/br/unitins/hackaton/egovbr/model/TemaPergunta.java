6f454447cbc91780a740a25a1ac7d43b59e43fa0
Kaio Macedo <kaiomm2000@gmail.com>
2023-10-20 01:27:51 -0300

    .

--- backend/hackaton-egovbr/src/main/java/br/unitins/hackaton/egovbr/model/TemaPergunta.java
+++ backend/hackaton-egovbr/src/main/java/br/unitins/hackaton/egovbr/model/TemaPergunta.java
@@ -1,22 +1,27 @@
 package br.unitins.hackaton.egovbr.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Inheritance(strategy = InheritanceType.JOINED)
-public class AvaliacaoExterna extends EntityClass {
-    @ManyToOne
-    @JoinColumn(name = "usuario_avaliacaoexterna")
-    private Usuario usuario;
+public class TemaPergunta extends EntityClass {
 
-    private AvaliacaoExternaResposta avaliacaoExternaResposta;
+    @Column(name = "nome")
+    private String nome;
+
+    @JoinColumn(name = "lista_pergunta_temaperguna")
+    @OneToMany
+    private List<Pergunta> perguntas;
 
 }
